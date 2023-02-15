a8d5ed089ac1e79cae0c247fa0fa2805c7727fbe
boltonlin <boltonlinbo@gmail.com>
2023-02-15 10:25:48 -0800

    final commit

---
+++ service/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./routes');
+
+const { LOCAL_URL, PORT } = process.env;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// FILL YOUR ROUTE
+app.use('*', router);
+
+app.listen(PORT);
+console.log(`Server listening at ${LOCAL_URL}:${PORT}`);
