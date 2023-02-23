7f90d21d353dfa39fbc226085e55f1ac243e0859
Pseudo08 <arpan.shrestha57@gmail.com>
2023-02-23 15:15:05 -0800

    Minor tweaks

--- service/cache/cache.js
+++ service/cache/cache.js
@@ -3,7 +3,7 @@ const Redis = require('ioredis') ;
 
 const redis = new Redis({
   'port': 6379,
-  'host': '127.0.0.1'
+  'host': 'localhost'
 })
 // const endpoint = `${DB_HOST}::${DB_PORT}/api/reviews/?product_id=80356`
 
