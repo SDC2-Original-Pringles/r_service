d88cacadddff81c95208228afd6cdf53b013e5ab
Pseudo08 <arpan.shrestha57@gmail.com>
2023-02-23 20:18:15 -0800

    redis diabled test

--- service/controllers/index.js
+++ service/controllers/index.js
@@ -13,17 +13,18 @@ module.exports = {
     } = req.query;
     page = page || 1;
     count = count || 5;
-    return redis.get(`getAll: ${[product_id, page, count, sort]}`)
-    .then((result) => {
-      if (result) {
-        result = JSON.parse(result);
-        return result;
-      } else {
-        return  models.getAll(product_id, page, count, sort)
-      }})
+    // return redis.get(`getAll: ${[product_id, page, count, sort]}`)
+    // .then((result) => {
+    //   if (result) {
+    //     result = JSON.parse(result);
+    //     return result;
+    //   } else {
+    //     return  models.getAll(product_id, page, count, sort)
+    //   }})
+    return models.getAll(product_id, page, count, sort)
     .then((result) => {
         // console.log(result);
-        redis.set(`getAll: ${[product_id, page, count, sort]}`, JSON.stringify(result), 'EX', 7200)
+        // redis.set(`getAll: ${[product_id, page, count, sort]}`, JSON.stringify(result), 'EX', 7200)
         res.status(200).send({product: product_id, page: page, count: count, results: result});
       })
       .catch((err) => {
@@ -32,16 +33,17 @@ module.exports = {
       });
   },
   getReviewMetaData: (req, res) => {
-    return redis.get(`getMeta: ${req.query.product_id}`)
-    .then((result)=> {
-      if (result) {
-        result = JSON.parse(result);
-        return result;
-      } else {
-        return models.getMeta(req.query.product_id);
-      }})
+    // return redis.get(`getMeta: ${req.query.product_id}`)
+    // .then((result)=> {
+    //   if (result) {
+    //     result = JSON.parse(result);
+    //     return result;
+    //   } else {
+    //     return models.getMeta(req.query.product_id);
+    //   }})
+    return models.getMeta(req.query.product_id)
     .then((result) => {
-        redis.set(`getMeta: ${req.query.product_id}`, JSON.stringify(result), 'EX', 3600);
+        // redis.set(`getMeta: ${req.query.product_id}`, JSON.stringify(result), 'EX', 3600);
         res.status(200).send(result);
       })
       .catch((err) => {
